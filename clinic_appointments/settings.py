7214c64de19c3db873462dc931203bedd97a96a1
ruslan-kornich <korny4rus143@gmail.com>
2023-01-12 12:37:57 +0200

    swagger docs

--- clinic_appointments/settings.py
+++ clinic_appointments/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework',
     'api.apps.ApiConfig',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- clinic_appointments/urls.py
+++ clinic_appointments/urls.py
@@ -1,10 +1,11 @@
-
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.documentation import include_docs_urls
+
+from .yasg import urlpatterns as doc_urls
+
 app_name = 'api'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
-    path("api/v1/docs/", include_docs_urls(),)
+    path('api/v1/', include('api.urls'))
 ]
+urlpatterns += doc_urls
